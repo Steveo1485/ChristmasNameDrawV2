e656e7e567aaec93244e86d9738abefef30e1089
Steven Nugent <stevennugent@gmail.com>
2014-11-21 18:10:56 -0800

    Skip pundit for about controller.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,5 +1,7 @@
 class AboutController < ApplicationController
   skip_before_action :authenticate_user!
+  skip_after_filter :verify_authorized
+  skip_after_filter :verify_policy_scoped
 
   def home
   end
