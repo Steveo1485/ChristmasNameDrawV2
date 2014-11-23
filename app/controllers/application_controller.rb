c545d08709e19dc9a37cd5be36b8d928e6484bf6
Steven Nugent <stevennugent@gmail.com>
2014-11-23 15:59:51 -0800

    Add extra fields to devise sign up.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name, :family_group]
+  end
 end
