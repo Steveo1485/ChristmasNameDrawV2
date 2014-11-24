4edc46293d933f631df8e060b4059df2a6f15f10
Steven Nugent <stevennugent@gmail.com>
2014-11-23 22:05:32 -0800

    Create list for user after user create.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe User, :type => :model do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
   it { should ensure_inclusion_of(:family_group).in_array(["Dragseth", "King", "Nugent"]) }
+
+  it "should create a list for user after create" do
+    @user = FactoryGirl.create(:user)
+    expect(@user.list).to be_valid
+  end
 end
